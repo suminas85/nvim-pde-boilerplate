c08671efd5999e428a819531fd06709156447d35
Andrei Sumin <suminas85@gmail.com>
2023-03-02 09:23:24 +0100

    #005 Lualine.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,25 +1,29 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = { 'nvim-tree/nvim-web-devicons' },
-		tag = 'nightly'
-	}
+  use 'wbthomason/packer.nvim'
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    tag = 'nightly'
+  }
 
-	-- color scheme
-	use 'sainnhe/everforest'
+  -- color scheme
+  use 'sainnhe/everforest'
 
-	-- text
-	use 'rmagatti/alternate-toggler'
-	use 'windwp/nvim-autopairs'
-	use 'mg979/vim-visual-multi'
-	use 'gcmt/wildfire.vim'
-	use 'tpope/vim-surround'
+  -- text
+  use 'rmagatti/alternate-toggler'
+  use 'windwp/nvim-autopairs'
+  use 'mg979/vim-visual-multi'
+  use 'gcmt/wildfire.vim'
+  use 'tpope/vim-surround'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
